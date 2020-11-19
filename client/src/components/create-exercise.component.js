4f95aef2a46038a652cc08f2e3f705e171637859
William Ray Noble Jr <wrnoblejrsbts@gmail.com>
2020-11-19 15:27:23 -0500

    resolved bug

--- client/src/components/create-exercise.component.js
+++ client/src/components/create-exercise.component.js
@@ -2,7 +2,6 @@ import React, { Component } from "react";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 import axios from "axios";
-import { response } from "express";
 
 export default class CreateExercise extends Component {
   constructor(props) {
