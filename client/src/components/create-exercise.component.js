fafea9a6a92c4c81af894f714e5ec691fc9dd375
William Ray Noble Jr <wrnoblejrsbts@gmail.com>
2020-11-19 12:27:44 -0500

    working on onChnage methods for create exercise

--- client/src/components/create-exercise.component.js
+++ client/src/components/create-exercise.component.js
@@ -1,6 +1,48 @@
 import React, { Component } from "react";
 
 export default class CreateExercise extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      username: "",
+      description: "",
+      duration: 0,
+      date: new Date(),
+      users: [],
+    };
+  }
+
+  onChangeUsername(e) {
+    this.setState({
+      username: e.target.value,
+    });
+  }
+
+  onChangeDescription(e) {
+    this.setState({
+      description: e.target.value,
+    });
+  }
+
+  onChangeDuration(e) {
+    this.setState({
+      duration: e.target.value,
+    });
+  }
+
+  onChangeDate(e) {
+    this.setState({
+      date: e.target.value,
+    });
+  }
+
+  onChangeUsers(e) {
+    this.setState({
+      users: e.target.value,
+    });
+  }
+
   render() {
     return (
       <div>
