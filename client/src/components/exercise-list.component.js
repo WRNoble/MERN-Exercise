df2bd20a2d0ebf1a6e13fea1b3eb79d1c5ba6986
William Ray Noble Jr <wrnoblejrsbts@gmail.com>
2020-11-19 15:39:06 -0500

    making progress on exercise list component

--- client/src/components/exercise-list.component.js
+++ client/src/components/exercise-list.component.js
@@ -1,6 +1,23 @@
 import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import axios from "axios";
 
 export default class ExerciseList extends Component {
+  constructor(props) {
+    super(props);
+
+    this.deleteExercise = this.deleteExercise.bind(this);
+
+    this.state = {
+      exercises: [],
+    };
+  }
+
+  componentDidMount() {
+    axios.get("http://localhost:5001/exercises/").then((response) => {
+      this.setState({ exercises: response.data });
+    });
+  }
   render() {
     return (
       <div>
