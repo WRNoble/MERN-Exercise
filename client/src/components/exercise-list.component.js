c9332b1a1fd6ac0f95143f8aacbefb4171cb75f8
William Ray Noble Jr <wrnoblejrsbts@gmail.com>
2020-11-19 16:02:27 -0500

    going to start on editing exercises

--- client/src/components/exercise-list.component.js
+++ client/src/components/exercise-list.component.js
@@ -2,6 +2,26 @@ import React, { Component } from "react";
 import { Link } from "react-router-dom";
 import axios from "axios";
 
+const Exercise = (props) => {
+  <tr>
+    <td>{props.exercise.username}</td>
+    <td>{props.exercise.description}</td>
+    <td>{props.exercise.duration}</td>
+    <td>{props.exercise.date.substring(0, 10)}</td>
+    <td>
+      <Link to={"/edit" + props.exercise._id}>Edit</Link> |{" "}
+      <a
+        href="#"
+        onClick={() => {
+          props.deleteExercise(props.exercise._id);
+        }}
+      >
+        Delete
+      </a>
+    </td>
+  </tr>;
+};
+
 export default class ExerciseList extends Component {
   constructor(props) {
     super(props);
@@ -34,10 +54,34 @@ export default class ExerciseList extends Component {
     });
   }
 
+  exerciseList() {
+    return this.state.exercises.map((currentexercise) => {
+      return (
+        <Exercise
+          exercise={currentexercise}
+          deleteExercise={this.deleteExercise}
+          key={currentexercise._id}
+        />
+      );
+    });
+  }
+
   render() {
     return (
       <div>
-        <p>Exercise List Component</p>
+        <h3>Logged Exercises</h3>
+        <table className="table">
+          <thead className="thead-light">
+            <tr>
+              <th>Username</th>
+              <th>Description</th>
+              <th>Duration</th>
+              <th>Date</th>
+              <th>Actions</th>
+            </tr>
+          </thead>
+          <tbody>{this.exerciseList()}</tbody>
+        </table>
       </div>
     );
   }
